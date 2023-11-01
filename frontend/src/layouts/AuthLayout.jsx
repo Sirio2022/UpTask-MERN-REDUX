15ee2db1302d6ccaad0eb7e352e7d57e1fedaae6
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-11-01 12:35:11 -0500

    Diseño login

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -3,8 +3,11 @@ import { Outlet } from 'react-router-dom';
 export default function AuthLayout() {
   return (
     <>
-      <div>AuthLayout</div>
-      <Outlet />
+      <main className="container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center">
+        <div className="md:w-2/3 lg:w-1/2 xl:w-3/5">
+          <Outlet />
+        </div>
+      </main>
     </>
   );
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,47 @@
-
-
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Inicia sesión y administra tus{' '}
+        <span className="text-slate-700">proyectos</span>{' '}
+      </h1>
+
+      <form className="mt-20 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label
+            htmlFor="email"
+            className="uppercase text-gray-600 block text-xl font-bold"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            placeholder="Email de registro"
+            id="email"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <div className="my-5">
+          <label
+            htmlFor="password"
+            className="uppercase text-gray-600 block text-xl font-bold"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            placeholder="Ingresa tu password"
+            id="password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Iniciar sesión"
+          className="bg-sky-600 w-full text-white uppercase py-3 mb-5 font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+    </>
+  );
 }
