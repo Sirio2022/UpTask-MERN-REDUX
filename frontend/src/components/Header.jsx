c74f51f81de45a0c0d1415e689dd2e7f0f81858f
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-11-12 18:10:21 -0500

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,9 +4,11 @@ export default function Header() {
   return (
     <header className="px-4 py-5 bg-white border-b">
       <div className="md:flex justify-between">
-        <h2 className="text-4xl text-sky-600 font-black text-center mb-5 md:mb-0">
-          UpTask
-        </h2>
+        <Link to="/proyectos">
+          <h2 className="text-4xl text-sky-600 font-black text-center mb-5 md:mb-0">
+            UpTask
+          </h2>
+        </Link>
         <div className="flex flex-col md:flex-row items-center gap-4">
           <button type="button" className="font-bold uppercase">
             Buscar Proyecto
